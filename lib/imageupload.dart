163b57fc61788275fc29c3b561c53e09338df705
alen130 <alenvarghese90@gmail.com>
2021-06-06 20:26:27 +0530

    deployed

--- lib/imageupload.dart
+++ lib/imageupload.dart
@@ -18,7 +18,8 @@ class ImageUploadPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
